39bbe75c5b07c6014c51e0f3248b071d1b5a0e1e
arunvangapelli1 <arun.vangapelli@blackboard.com>
2015-12-30 14:34:19 -0700

    test comment

--- user/usercardholders-v1/src/main/java/com/bbtransact/icp/api/resource/ICardHolderIssuerRepository.java
+++ user/usercardholders-v1/src/main/java/com/bbtransact/icp/api/resource/ICardHolderIssuerRepository.java
@@ -3,6 +3,7 @@ package com.bbtransact.icp.api.resource;
 import com.bbtransact.icp.api.shared.bean.CardHolderIssuerBean;
 import com.bbtransact.icp.api.shared.bean.OperationsBean;
 
+//comment test
 public interface ICardHolderIssuerRepository {
     public OperationsBean getCardHolderIssuerOperations(String id, String requestTimeStamp);
 
